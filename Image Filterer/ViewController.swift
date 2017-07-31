064166501e9295a48547a8abf51711ec311eedd1
Nguyen Trung <nguyentrung0904@gmail.com>
2017-07-31 20:48:09 +0700

    Initial Commit

---
+++ Image Filterer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image Filterer
+//
+//  Created by Nguyen Bui An Trung on 31/7/17.
+//  Copyright © 2017 Nguyen Bui An Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
